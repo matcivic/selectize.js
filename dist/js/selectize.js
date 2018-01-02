b8a46022d88089c0d4b39368f87dfda03a1f6193
ZION\Mateusz.Koguc <mateusz.koguc@civica.co.uk>
2018-01-02 17:28:46 +0000

    IE fix in dist

--- dist/js/selectize.js
+++ dist/js/selectize.js
@@ -685,7 +685,6 @@
 				keyup     : function() { return self.onKeyUp.apply(self, arguments); },
 				keypress  : function() { return self.onKeyPress.apply(self, arguments); },
 				resize    : function() { self.positionDropdown.apply(self, []); },
-				blur      : function() { return self.onBlur.apply(self, arguments); },
 				focus     : function() { self.ignoreBlur = false; return self.onFocus.apply(self, arguments); },
 				paste     : function() { return self.onPaste.apply(self, arguments); }
 			});
